5cc556c9d8948686b817f7c4dc2019ccce10cfff
sabyrzhanzhaslanuly7 <sabyrzhanzhaslanuly7@gmail.com>
2025-08-28 14:30:34 +0500

    feat: add js file with data, ui and logic inside

---
+++ js/data.js
@@ -0,0 +1,10 @@
+export const tracks = [
+	{
+		title: 'Musicfun soundtrack',
+		url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3',
+	},
+	{
+		title: 'Musicfun soundtrack instrumental',
+		url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3',
+	},
+]
